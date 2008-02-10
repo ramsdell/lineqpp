4ac788de05ea337f3c00b4d3e479aec7c9cdb80f
John Ramsdell <ramsdell@luaforge.net>
2008-02-10 00:37:44 +0000

    first translations

--- lineqpp/lineqpp.lua
+++ lineqpp/lineqpp.lua
@@ -1,37 +1,34 @@
+-- Say something about error messages
 
+-- message level
 
-local function display(s)
-   if verbose then
-      return io.stderr:write(s)
-   end
+local level = 0
+
+local function err(msg)
+   error(msg, level)
 end
 
-local function show(t)
-   if type(t) == "table" and not getmetatable(t) then
-      if #t == 0 then
-	 return display("()")
-      end
-      display("(")
-      for i = 1, #t - 1 do
-	 show(t[i])
-	 display(" ")
-      end
-      show(t[#t])
-      return display(")")
-   else 
-      return display(tostring(t))
-   end
+-- Say something about verbose
+
+local function display(s)
+   return io.stderr:write(s)
 end
 
 -- Real numbers
 
+local tol = 1.0e-6
+
 local function is_zero(x)
-   local tol = 1.0e-6
-   if x < 0 then
-      return x > -tol
+   return math.abs(x) < tol
+end
+
+-- Replace numbers close to zero with zero.
+local function zero(x)
+   if is_zero(x) then
+      return 0
    else
-      return x < tol
-   end
+      return x
+   end   
 end
 
 -- Complex numbers
@@ -41,6 +38,11 @@ Complex.__index = Complex
 
 local function complex(x, y)
    return setmetatable({r = x, i = y or 0}, Complex)
+end
+
+-- A simple distance metric that is quick to compute
+function Complex.mag(z)
+   return math.max(math.abs(z.r), math.abs(z.i))
 end
 
 function Complex.is_zero(z)
@@ -81,29 +83,32 @@ function Complex.__div(z1, z2)
 end
 
 -- __pow should at least support squaring.
-
--- Printing
-
-function Complex.imaginary_tostring(z)
-   if is_zero(z.i - 1) then
-      return "i"
-   else
-      return z.i..'*i'
-   end
+function Complex.__pow(z1, z2)
+   err("exponention is broken")
 end
 
+-- To string method
+
 function Complex.__tostring(z)
+   local function imaginary_tostring(z)
+      if is_zero(z.i - 1) then
+	 return "i"
+      else
+	 return z.i..'*i'
+      end
+   end
    if is_zero(z.i) then
-      return tostring(z.r)
+      return tostring(zero(z.r))
    elseif is_zero(z.r) then
-      return z:imaginary_tostring()
+      return imaginary_tostring(z)
    elseif is_zero(z.i + 1) then
       return z.r..' - i'
    else
-      return z.r..' + '..z:imaginary_tostring()
+      return z.r..' + '..imaginary_tostring(z)
    end
 end
 
+-- Is a sum required to print this number?
 function Complex.is_sum(z)
    return not is_zero(z.r) and not is_zero(z.i)
 end
@@ -114,6 +119,19 @@ end
 -- and produce one.
 
 local func = {}
+
+-- Translation table
+
+local translation = {}
+
+local function solved(v, z)
+   translation[v.."#r"] = tostring(zero(z.r));
+   translation[v.."#i"] = tostring(zero(z.i)));
+end
+
+function translate(s)
+   return translation[s]
+end
 
 -- Linear polynomials (polynomials with degree one or zero)
 
@@ -128,6 +146,23 @@ local func = {}
 
 local Linear = {}
 Linear.__index = Linear
+
+local function linear(c, ts)
+   return setmetatable({c = c, ts = ts or {}}, Linear)
+end
+
+-- Delete linear terms with zero coefficients
+function Linear.simplify(p)
+   local a = {}			-- Collect terms to be deleted
+   for v, c in pairs(p.ts) do
+      if c:is_zero() then
+	 a[#a + 1] = v
+      end
+   end
+   for i in ipairs(a) do	-- Delete collected terms
+      p.ts[a[i]] = nil
+   end
+end
 
 -- Compute the number of entries in a table
 local function size(t)
@@ -153,6 +188,104 @@ function Linear.variable(p)
       end
    end
    return false
+end
+
+function Linear.__add(p1, p2)
+   local ts = {}
+   for v1, c1 in pairs(p1.ts) do
+      ts[v1] = c1
+   end
+   for v2, c2 in pairs(p2.ts) do
+      local c1 = ts[v2]
+      if c1 then
+	 ts[v2] = c1 + c2
+      else
+	 ts[v2] = c2
+      end
+   end
+   return linear(p1.c + p2.c, ts)
+end
+   
+function Linear.__sub(p1, p2)
+   local ts = {}
+   for v1, c1 in pairs(p1.ts) do
+      ts[v1] = c1
+   end
+   for v2, c2 in pairs(p2.ts) do
+      local c1 = ts[v2]
+      if c1 then
+	 ts[v2] = c1 - c2
+      else
+	 ts[v2] = -c2
+      end
+   end
+   return linear(p1.c - p2.c, ts)
+end
+
+function Linear.__umn(p)
+   local ts = {}
+   for v, c in pairs(p.ts) do
+      ts[v] = -c
+   end
+   return linear(-p.c, ts)
+end
+
+function Linear.__mul(p1, p2)
+   local c1 = p1:number()
+   if not c1 then
+      c1 = p2:number()
+      if not c1 then
+	 err("both in product not a number")
+      else
+	 p2 = p1
+      end
+   end
+   local ts = {}
+   for v2, c2 in pairs(p2.ts) do
+      ts[v2] = c1 * c2
+   end
+   return linear(c1 * p2.c, ts)
+end
+
+function Linear.__div(p1, p2)
+   local c2 = p2:number()
+   if not c2 then
+      err("dividend is not a number")
+   end
+   if is_zero(c2) then
+      c2 = tol
+   end
+   return linear(complex(1) / c2) * p1
+end
+
+function Linear.__pow(p1, p2)
+   local c1, c2 = p1:number(), p2:number()
+   if not c1 then
+      err("exponent base not a number") -- FIX ME
+   elseif not c2 then
+      err("exponent not a number")
+   else
+      return linear(c1 ^ c2)
+   end
+end
+
+function Linear.subst(p1, v1, p2, v2)
+   local c1 = p1.ts[v2]
+   if not c1 then
+      return p1
+   else
+      p1.ts[v2] = nil
+      p1 = p1 + linear(c1) * p2
+      p1:simplify()
+      if verbose then
+	 display(v1.." is "..tostring(p1).."\n")
+      end
+      c1 = p1:number()
+      if c1 then
+	 solved(v1, c1)
+      end
+      return p1
+   end
 end
 
 -- Sorted pairs iterator from PIL 2nd Ed, pg. 173.
@@ -194,19 +327,51 @@ end
 
 local env = {}
 
+local function solve(p)
+   p:simplify()
+   local c = p:number()
+   if c then
+      if c:is_zero() then
+	 err("redundant equation")
+      else
+	 err("inconsistent equation")
+      end
+   end
+   local m_max, v_max, c_max = 0
+   for v, c in pairs(p.ts) do
+      local m = c:mag()
+      if m > m_max then
+	 m_max, v_max, c_max = m, v, c
+      end
+   end
+   p.ts[v_max] = nil
+   c_max = complex(-1) / c_max
+   p = linear(c_max) * p
+   p:simplify()
+   if verbose then
+      display(v_max.." is "..tostring(p).."\n")
+   end
+   c = p:number()
+   if c then
+      solved(v_max, c)
+   end
+   for ve, pe in pairs(env) do
+      env[ve] = pe:subst(ve, p, v_max)
+   end
+   env[v_max] = p
+end   
+
 function variable(var)
    local val = env[var]
    if val then
       return val
    else
-      local p = {c = complex(0), ts = {[var] = complex(1)}}
-      return setmetatable(p, Linear)
+      return linear(complex(0), {[var] = complex(1)})
    end
 end
 
 function number(x, y)
-   local p = {c = complex(x, y), ts = {}}
-   return setmetatable(p, Linear)
+   return linear(complex(x, y))
 end
 
 env.i = number(0, 1)
@@ -216,61 +381,51 @@ function application(fun, arg)
    local val = func[var]
    local num = arg:number()
    if not var then
-      error("function not well formed")
+      err("function not well formed")
    elseif not val then
-      error("function "..var.." not defined")
+      err("function "..var.." not defined")
    elseif not num then
-      error("function "..var.." not applied to a number")
+      err("function "..var.." not applied to a number")
    else
-      local ans = val(num)
-      return number(ans.r, ans.i)
+      return linear(val(num))
    end
 end
 
 -- x [y, z] = y + t * (z - y)
 function mediation(scale, left, right)
-   return {"med", scale, left, right}
+   return left + scale * (right - left)
 end
 
 function sum(left, right)
-   return {"plus", left, right}
+   return left + right
 end
 
 function difference(left, right)
-   return {"sub", left, right}
+   return left - right
 end
 
 function product(left, right)
-   return {"mul", left, right}
+   return left * right
 end
 
 function quotient(left, right)
-   return {"div", left, right}
+   return left / right
 end
 
 function negation(arg)
-   return {"neg", arg}
+   return -arg
 end
 
 function exponentiation(left, right)
-   return {"exp", left, right}
+   return left ^ right
 end
 
 function equation(left, right)
-   show(left)
-   display("\n")
-   show(right)
-   display("\n\n")
+   if verbose then
+      left:simplify()
+      right:simplify()
+      display(tostring(left).." = "..tostring(right).."\n")
+   end
+   solve(left - right)
    return right
 end
-
-function translate(s)
-   return nil
-end
-
--- Debuging hacks
-
-cpx = complex
-var = variable
-num = number
-
